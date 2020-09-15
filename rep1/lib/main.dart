3b1c6169cab64701c1ed913582ab31aeb0ec315d
ahmed_arnaout <ahmed.arnaout123@gmail.com>
2020-09-16 02:13:27 +0300

    Initialize flutter project

---
+++ rep1/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [],
+      ),
+    );
+  }
+}
